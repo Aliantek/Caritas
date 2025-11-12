6d34a4b489e72e406552904b98d75f2237863252
Corian2108 <jefferson.ruiz2108@gmail.com>
2025-11-12 16:43:25 -0500

    update/ se aplican estilos primarios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
+import { BttpComponent } from '../../shared/bttp/bttp.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent, AboutComponent, ContactComponent],
+  imports: [HeaderComponent, FooterComponent, AboutComponent, ContactComponent, BttpComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
